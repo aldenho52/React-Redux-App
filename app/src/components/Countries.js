703846fd02ce995e63166b3132ec97c0c28c8753
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-04 18:04:12 -0700

    added global header

--- app/src/components/Countries.js
+++ app/src/components/Countries.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchCountries } from '../actions'
 import GlobalHeader from './GlobalHeader'
 
 const Countries = (props) => {
+    const [searchText, setSearchText] = useState('')
+
+    const onChangeHandler = (e) => {
+        setSearchText(e.target.value)
+    }
+
     useEffect(()=> {
         props.fetchCountries()
     }, [])
@@ -12,12 +18,19 @@ const Countries = (props) => {
     return (
         <div>
             <h1>Covid-19 Daily Summary</h1>
-            <GlobalHeader globalData={props.global} />
             {props.isLoading ? <p>Loading covid-19 countryData...</p> : null}
             {props.error ? <p style={{ color: 'red' }}>{props.error}</p> : null}
+
+            <GlobalHeader globalData={props.global} />
+            <input
+                type='text' 
+                placeholder='search country' 
+                onChange={onChangeHandler}
+                value={searchText}
+                 />
             <div className='list-container'>
                 {props.countryData.map(item => (
-                    <p key={item.id}>{item.Country}</p>
+                    <p className='item' key={item.id}>{item.Country}</p>
                 ))}
             </div>
             
